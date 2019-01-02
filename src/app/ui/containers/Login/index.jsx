8b97a7b3b2046189ee31d491acdc462db1f59db2
Luca Pierfederici <lpier97@gmail.com>
2019-01-02 14:28:32 +0100

    login and dashboard pages

--- src/app/ui/containers/Login/index.jsx
+++ src/app/ui/containers/Login/index.jsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 
-export class Dashboard extends React.Component {
+export class Login extends React.Component {
   render() {
     return (
       <div>
-        Dashboard
+        Login
       </div>
     );
   }
