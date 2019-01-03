14adcb6ee44c666f796c9f7d10ef67d0d25b53c9
Luca Pierfederici <lpier97@gmail.com>
2019-01-03 11:29:57 +0100

    register page

--- src/app/ui/containers/Register/index.jsx
+++ src/app/ui/containers/Register/index.jsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
+import UserForm from '../../components/UserForm';
+import {Card} from '../../components/elements/Card';
 
 export class Register extends React.Component {
   render() {
     return (
-      <div>
-        Register
-      </div>
+      <Card>
+        <UserForm type="register"/>
+      </Card>
     );
   }
 }
