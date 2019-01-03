ecde2e457def20a400af2a563238691fbd67d4aa
Luca Pierfederici <lpier97@gmail.com>
2019-01-03 11:38:24 +0100

    dashboard page

--- src/app/ui/containers/Dashboard/index.jsx
+++ src/app/ui/containers/Dashboard/index.jsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
+import { Button } from '../../components/elements/Button';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { UserAction } from '../../../store/user/user.actions';
 
-export class Dashboard extends React.Component {
+class Dashboard extends React.Component {
   render() {
     return (
       <div>
-        Dashboard
+        Benvenuto { this.props.user.email }
+        <Button onClick={this.props.logout}>Esci</Button>
       </div>
     );
   }
 }
+
+export default connect(
+  (state) => ({
+    user: state.user
+  }),
+  (dispatch) => {
+    return ({
+      logout: bindActionCreators(UserAction, dispatch).logoutUser,
+    })
+  }
+)(Dashboard)
