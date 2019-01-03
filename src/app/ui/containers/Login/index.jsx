fe617e215ab0f390b23a7febdeea031461f02080
Luca Pierfederici <lpier97@gmail.com>
2019-01-03 11:30:14 +0100

    login page with form

--- src/app/ui/containers/Login/index.jsx
+++ src/app/ui/containers/Login/index.jsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import UserForm from '../../components/UserForm';
+import {Card} from '../../components/elements/Card';
 
 export class Login extends React.Component {
   render() {
     return (
-      <div>
-        <UserForm/>
-      </div>
+      <Card>
+        <UserForm type="login"/>
+      </Card>
     );
   }
 }
