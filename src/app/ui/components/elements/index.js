ec30ac2af43858bb60e646c4fd3040f9d37fdda8
Luca Pierfederici <lpier97@gmail.com>
2019-01-03 14:13:56 +0100

    Index for elements

---
+++ src/app/ui/components/elements/index.js
@@ -0,0 +1,7 @@
+import { Button } from './Button';
+import { Card } from './Card'
+import { ErrorMessage } from './ErrorMessage'
+import { Input } from './Input'
+import { Link } from './Link'
+
+export { Button, Card, ErrorMessage, Input, Link }
